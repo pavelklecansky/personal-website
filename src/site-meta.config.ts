9c6ed8c4e8f20c9a01eadc5b0a14b836e8641e6a
Pavel Klečanský <klecanskypavel@gmail.com>
2022-11-16 16:35:41 +0100

    Basic configuration

--- src/site-meta.config.ts
+++ src/site-meta.config.ts
@@ -3,7 +3,7 @@ const siteMeta = {
   description: "An opinionated starter theme for Astro",
   lang: "en-GB",
   ogLocale: "en_GB",
-  githubUrl: "https://github.com/chrismwilliams/astro-theme-cactus",
+  githubUrl: "https://github.com/pavelklecansky/personal-website",
   themeColorLight: "#fafafa",
   themeColorDark: "#1d1f21",
 }
