49425bf8764745db7657e2ecdd880e0627da7489
Pavel Klečanský <klecanskypavel@gmail.com>
2023-05-25 15:46:08 +0200

    Add Linkedin as Social link and Google analytics.

--- src/data/constants.ts
+++ src/data/constants.ts
@@ -16,5 +16,6 @@ export const MENU_LINKS = [
 // ! Remember to add your own socials
 export const SOCIAL_LINKS = {
 	github: "https://github.com/pavelklecansky",
+	linkedin: "https://www.linkedin.com/in/pavel-klecansky/",
 	email: "pavel@klecansky.cz",
 };
