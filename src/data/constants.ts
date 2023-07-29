ed12b967c5dbcc5ca9db334738b0c7c6e04cf2d0
Pavel Klečanský <klecanskypavel@gmail.com>
2023-07-29 22:14:44 +0200

    Add new translation and change some styles.

--- src/data/constants.ts
+++ src/data/constants.ts
@@ -57,40 +57,11 @@ export const PLAN_TO_LEARN = [
 ];
 
 export const PROJECTS: Array<{ title: string; desc: string; href: string }> = [
+
 	{
-		title: "Astro",
-		desc: "Build fast websites, faster.",
-		href: "https://astro.build",
-	},
-	{
-		title: "Astro Assets",
-		desc: "Built-in optimized asset support.",
-		href: "https://docs.astro.build/en/guides/assets/",
-	},
-	{
-		title: "Tailwind CSS",
-		desc: "Rapidly build modern websites without ever leaving your HTML.",
-		href: "https://tailwindcss.com",
-	},
-	{
-		title: "Markdown",
-		desc: "Simple and easy-to-use markup language.",
-		href: "https://www.markdownguide.org/",
-	},
-	{
-		title: "MDX",
-		desc: "Markdown for the component era.",
-		href: "https://mdxjs.com/",
-	},
-	{
-		title: "Satori",
-		desc: "Generating png Open Graph images for blog posts.",
-		href: "https://github.com/vercel/satori",
-	},
-	{
-		title: "Pagefind",
-		desc: "A fully static search library.",
-		href: "https://pagefind.app/",
+		title: "project.wren.title",
+		desc: "project.wren.description",
+		href: "https://github.com/pavelklecansky/Wren_Interpreter",
 	},
 ];
 
